8b04dc17e4e562fafd8137ad62f9059fcbcac19b
Uddeshya002 <ukumar_be24@thapar.edu>
2026-04-19 05:08:27 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Apply from "./pages/Apply";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/apply" element={<Apply />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
